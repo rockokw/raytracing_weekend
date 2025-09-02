5dedf99d3de2c17924b703e4b401e8cc0d3573a9
Kwame Wright <rockokw@gmail.com>
2025-09-02 07:45:03 -0700

    Ray-sphere intersection

--- src/weekend.cc
+++ src/weekend.cc
@@ -5,7 +5,20 @@
 
 using std::max;
 
+bool hit_sphere(const Point3& center, double radius, const Ray& r) {
+  Vec3 oc = center - r.origin();
+  auto a = Dot(r.direction(), r.direction());
+  auto b = -2.0 * Dot(r.direction(), oc);
+  auto c = Dot(oc, oc) - radius * radius;
+  auto discriminant = (b * b) - (4 * a * c);
+  return (discriminant >= 0);
+}
+
 Color RayColor(const Ray& r) {
+  if (hit_sphere(Point3(0, 0, -1), 0.5, r)) {
+    return Color(1, 0, 0);
+  }
+
   Vec3 unit_direction = UnitVector(r.direction());
   auto a = 0.5 * (unit_direction.y() + 1.0);
 
